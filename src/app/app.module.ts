1451c07fd4cba891f627f678f706fe98b00204bc
Gloria Friesen <gloriafriesen@gmail.com>
2017-08-05 17:32:28 -0700

    add html form elements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,17 @@
+import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { FormsModule }   from '@angular/forms';
 
-import { AppComponent } from './app.component';
+import { AppComponent }  from './app.component';
 
 @NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
