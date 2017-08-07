444a401ab6346ad7725c475deee0285b5d168c9d
Gloria Friesen <gloriafriesen@gmail.com>
2017-08-06 20:18:16 -0700

    implement form validation library, show error and success message on input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CustomValidators } from 'ng2-validation';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  input = {
-    url: ' '
+export class AppComponent implements OnInit {
+
+
+  newForm: FormGroup;
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit() {
+    this.newForm = this.fb.group({
+      websiteLink: ['', Validators.required]
+    })
   }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,17 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
+import { CustomFormsModule } from 'ng2-validation'
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent }  from './app.component';
 
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    CustomFormsModule,
+    ReactiveFormsModule
   ],
   declarations: [
     AppComponent
